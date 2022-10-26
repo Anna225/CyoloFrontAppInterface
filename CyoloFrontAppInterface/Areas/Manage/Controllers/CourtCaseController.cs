6b67d9bcb0b0e1d341b949f2ed2433c15c183d3a
mohitt2d <104139311+mohitt2d@users.noreply.github.com>
2022-10-26 23:15:39 +0000

    'init'

---
+++ CyoloFrontAppInterface/Areas/Manage/Controllers/CourtCaseController.cs
@@ -0,0 +1,138 @@
+﻿using CyoloFrontAppInterface.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CyoloFrontAppInterface.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class CourtCaseController : Controller
+    {
+
+        // GET: CourtCaseController
+        public async Task<ActionResult> Index(string date, string email = "walter.damen@advocatenkantoordamen.be")
+        {
+            if (date == null)
+            {
+                date = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+            BackendServerAPI ls = new BackendServerAPI();
+            try
+            {
+                ViewBag.Model = await ls.GetCourtCaseByEmailAndDate(email, date);
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Model = null;
+                Console.WriteLine(ex);
+            }
+            
+            ViewBag.Today = date;
+            ViewBag.Email = email;
+            return View();
+        }
+
+        // GET: CourtCaseController/GetByEmailAndDate
+        public async Task<ActionResult> GetByEmailAndDate(string date, string name)
+        {
+            if (date == null)
+            {
+                date = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+            BackendServerAPI ls = new BackendServerAPI();
+            try
+            {
+                ViewBag.Model = await ls.GetCourtCaseByNameAndDate(name, date);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Model = null;
+                Console.WriteLine(ex);
+            }
+
+            ViewBag.Today = date;
+            ViewBag.Email = name;
+            return View();
+        }
+
+        // GET: CourtCaseController/Match
+        public async Task<ActionResult> Match(string courtCaseNo)
+        {
+            BackendServerAPI ls = new BackendServerAPI();
+            ViewBag.CourtTypes = await ls.GetAllCourtTypes();
+            ViewBag.CourtLocations = await ls.GetAllCourtLocations();
+            ViewBag.ChamberIds = await ls.GetAllChamberIDs();
+            ViewBag.Model = await ls.GetLawyersByCourtcaseno(courtCaseNo);
+            return View();
+        }
+
+        // GET: CourtCaseController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CourtCaseController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CourtCaseController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourtCaseController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CourtCaseController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourtCaseController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CourtCaseController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CyoloFrontAppInterface/Data/BackendServerAPI.cs
@@ -0,0 +1,69 @@
+﻿#nullable disable
+using Newtonsoft.Json;
+using RestSharp;
+using System.Linq.Expressions;
+
+namespace CyoloFrontAppInterface.Data
+{
+    public class BackendServerAPI
+    {
+        private RestClient _client;
+        public BackendServerAPI()
+        {
+            _client = new RestClient("https://cyoloapigateway.azure-api.net");
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public async Task<dynamic> GetCourtCaseByEmailAndDate(string email, string date)
+        {
+            var request = new RestRequest($"/api/Custom/CourtCaseByDateAndEmail/{date}/{email}");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+
+        public async Task<dynamic> GetCourtCaseByNameAndDate(string lawyername, string date)
+        {
+            var request = new RestRequest($"/api/Custom/CourtCaseByDateAndName/{date}/{lawyername}");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+
+        public async Task<dynamic> GetLawyersByCourtcaseno(string courtcaseno)
+        {
+            var request = new RestRequest($"/api/Custom/LawyersByCourtCaseId/{courtcaseno}");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+
+        public async Task<dynamic> GetAllCourtTypes()
+        {
+            var request = new RestRequest($"/api/Custom/AllCourtTypes");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+
+        public async Task<dynamic> GetAllCourtLocations()
+        {
+            var request = new RestRequest($"/api/Custom/AllCourtLocations");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+        public async Task<dynamic> GetAllChamberIDs()
+        {
+            var request = new RestRequest($"/api/Custom/AllChamberIDs");
+            request.AddHeader("ocp-apim-subscription-key", "d23d9c7c11da4b228417e567c85fa80c");
+            var response = await _client.GetAsync(request);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+
+    }
+}
