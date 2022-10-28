f1511b973f9676fdaedb9187fdcd44d8eda2b521
mohitt2d <104139311+mohitt2d@users.noreply.github.com>
2022-10-28 16:35:38 +0000

    last2022-10-28

--- CyoloFrontAppInterface/Models/CourtCaseAgenda.cs
+++ CyoloFrontAppInterface/Models/CourtCaseAgenda.cs
@@ -5,7 +5,8 @@
         public int ID { get; set; }
         public string? CourtCaseNo { get; set; }
         public string? HearingGeneral { get; set; }
-        public string? HearingDateTime { get; set; }
+        public string? HearingDate { get; set; }
+        public string? HearingTime { get; set; }
         public string? HearingType { get; set; }
         public string? CourtType { get; set; }
         public string? ChamberID { get; set; }
